b1bb5a7084b3e299fe84860caae26c2af0d336cf
yeolife <yeo2507@naver.com>
2025-02-05 09:00:39 +0900

    feat. 원격과 로컬 Module 분리

--- app/src/main/java/com/example/soopimageloader/di/LocalModule.kt
+++ app/src/main/java/com/example/soopimageloader/di/LocalModule.kt
@@ -6,24 +6,12 @@ import com.example.soopimageloader.data.local.CategoryDao
 import com.example.soopimageloader.data.local.CategoryDatabase
 import com.example.soopimageloader.data.local.CategoryLocalDataSource
 import com.example.soopimageloader.data.local.CategoryLocalDataSourceImpl
-import com.example.soopimageloader.data.remote.CategoryAPI
-import com.example.soopimageloader.data.remote.CategoryRemoteDataSource
-import com.example.soopimageloader.data.remote.CategoryRemoteDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
-@Module
-@InstallIn(SingletonComponent::class)
-object RemoteModule {
-    @Provides
-    @Singleton
-    fun provideCategoryRemoteDataSource(api: CategoryAPI): CategoryRemoteDataSource =
-        CategoryRemoteDataSourceImpl(api)
-}
 
 @Module
 @InstallIn(SingletonComponent::class)
